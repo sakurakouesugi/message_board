5bc83fc58537c7e77d91a77d423355ae1a9422fe
sakurako.uesugi <cocb.1d2hznll.2525@gmail.com>
2021-12-12 21:04:07 +0900

    NewServlet.javaを作成

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,17 +1,21 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Message;
+import utils.DBUtil;
 /**
  * Servlet implementation class IndexServlet
  */
-@WebServlet(name = "index", urlPatterns = { "/index" })
+@WebServlet("/index" )
 public class IndexServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -27,8 +31,13 @@ public class IndexServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+        EntityManager em = DBUtil.createEntityManager();
+
+        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
+        response.getWriter().append(Integer.valueOf(messages.size()).toString());
+
+        em.close();
     }
+
 
 }
